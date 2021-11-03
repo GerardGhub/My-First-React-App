f2426c5c30f32bd5f68504925d0683892d4a5e65
GerardGHub <singian28@gmail.com>
2021-11-03 23:26:28 +0800

    My First React App

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+var element = <button class="btn btn-danger">hai</button>;
+ReactDOM.render(element, document.getElementById("root"));
