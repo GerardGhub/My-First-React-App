6c84dfbb2da5f249e87294eb934dd2c7667c057d
GerardGHub <singian28@gmail.com>
2021-11-07 00:33:35 +0800

    Render Methods

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -31,7 +31,7 @@ export default class MainContent extends Component {
   render() {
     return (
       <div>
-        <h4 className="border-bottom m-1 p-1">
+        <h4 className="m-1 p-1">
           {this.state.pageTitle}
 
           <span className="badge badge-secondary m-2">
@@ -50,24 +50,7 @@ export default class MainContent extends Component {
               <th>City</th>
             </tr>
           </thead>
-          <tbody>
-            {this.state.customers.map((cust) => {
-              return (
-                <tr key={cust.id}>
-                  <td>{cust.id}</td>
-                  <td>{cust.name}</td>
-                  <td>
-                    {cust.phone ? (
-                      cust.phone
-                    ) : (
-                      <div className="bg-warning p-2 text-center">No Phone</div>
-                    )}
-                  </td>
-                  <td>{cust.address.city}</td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <tbody>{this.getCustomerRow()}</tbody>
         </table>
       </div>
     );
@@ -76,6 +59,24 @@ export default class MainContent extends Component {
   onRefreshClick = () => {
     this.setState({
       customersCount: 7,
+    });
+  };
+  getPhoneToRender = (phone) => {
+    if (phone) return phone;
+    else {
+      return <div className="bg-warning p-2 text-center">No Phone</div>;
+    }
+  };
+  getCustomerRow = () => {
+    return this.state.customers.map((cust) => {
+      return (
+        <tr key={cust.id}>
+          <td>{cust.id}</td>
+          <td>{cust.name}</td>
+          <td>{this.getPhoneToRender(cust.phone)}</td>
+          <td>{cust.address.city}</td>
+        </tr>
+      );
     });
   };
 }
