81755a7b5539e743a54debbb931c8f3a58b65628
GerardGHub <singian28@gmail.com>
2021-11-07 00:06:05 +0800

    Render List

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
 
 export default class MainContent extends Component {
-  state = { pageTitle: "Customers", customersCount: 5 };
+  state = {
+    pageTitle: "Customers",
+    customersCount: 5,
+    customers: [
+      {
+        id: 1,
+        name: "Scott",
+        phone: "153-456",
+        address: { city: "New Delhi" },
+      },
+      {
+        id: 2,
+        name: "Jilo",
+        phone: "111-456",
+        address: { city: "New Jersey" },
+      },
+      {
+        id: 3,
+        name: "Peter",
+        phone: "143-456",
+        address: { city: "New London" },
+      },
+      { id: 4, name: "Gerard", phone: "163-456", address: { city: "Berlin" } },
+      { id: 5, name: "Vilo", phone: "333-456", address: { city: "New York" } },
+    ],
+  };
 
   render() {
     return (
@@ -16,6 +41,28 @@ export default class MainContent extends Component {
             Refresh
           </button>
         </h4>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Customer Name</th>
+              <th>Phone</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.customers.map((cust) => {
+              return (
+                <tr key={cust.id}>
+                  <td>{cust.id}</td>
+                  <td>{cust.name}</td>
+                  <td>{cust.phone}</td>
+                  <td>{cust.address.city}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
