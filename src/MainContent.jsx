8e7e53737ae51328a2148136c75551f11af1ce92
GerardGHub <singian28@gmail.com>
2021-11-07 22:29:37 +0800

    Passs Events Arguments

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -43,12 +43,6 @@ export default class MainContent extends Component {
     ],
   };
 
-  customerNameStyle = (custName) => {
-    if (custName.startsWith("S")) return "green-highlight border-left";
-    else if (custName.startsWith("G")) return "red-highlight border-right";
-    else return "";
-  };
-
   render() {
     return (
       <div>
@@ -90,18 +84,36 @@ export default class MainContent extends Component {
     }
   };
   getCustomerRow = () => {
-    return this.state.customers.map((cust) => {
+    return this.state.customers.map((cust, index) => {
       return (
         <tr key={cust.id}>
           <td>{cust.id}</td>
           <td>
             <img src={cust.photo} alt="Customer" />
+            <div>
+              <button
+                className="btn btn-sm btn-secondary"
+                onClick={() => {
+                  this.onChangePictureClick(cust, index);
+                }}
+              >
+                Change Picture
+              </button>
+            </div>
           </td>
-          <td className={this.customerNameStyle(cust.name)}>{cust.name}</td>
+          <td>{cust.name}</td>
           <td>{this.getPhoneToRender(cust.phone)}</td>
           <td>{cust.address.city}</td>
         </tr>
       );
     });
+  };
+
+  onChangePictureClick = (cust, index) => {
+    //console.log(cust);
+    //console.log(index);
+    var custArr = this.state.customers;
+    custArr[index].photo = "https://picsum.photos/id/104/60";
+    this.setState({ customers: custArr });
   };
 }
