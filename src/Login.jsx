40ba41a6786215f582ac62a141602fefb08a8951
GerardGHub <singian28@gmail.com>
2021-11-27 22:44:29 +0800

    Form Submit Button

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { email: "abc@test.com", password: "abc123" };
+    this.state = { email: "", password: "", message: "" };
   }
   render() {
     return (
@@ -37,13 +37,32 @@ export default class Login extends Component {
           />
         </div>
         {/* End of Password */}
-        <div className="btn btn-primary" onClick={this.onLoginClick}>
-          Login
+        <div className="text-right">
+          {this.state.message}
+          <button className="btn btn-primary m-1" onClick={this.onLoginClick}>
+            Login
+          </button>
         </div>
       </div>
     );
   } //end of render
   onLoginClick = () => {
     console.log(this.state);
+    if (
+      this.state.email === "admin@test.com" &&
+      this.state.password === "admin123"
+    ) {
+      //Success
+      this.setState({
+        message: <span className="text-success"> Successfully Logged-in</span>,
+      });
+    } else {
+      //Error
+      this.setState({
+        message: (
+          <span className="text-danger"> Invalid Login , Please Try Again</span>
+        ),
+      });
+    }
   };
 }
