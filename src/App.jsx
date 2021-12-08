b77deda6df909e604bd9393d2064eaea0874c9cf
gerard singian <gerard.singian@outlook.com>
2021-12-08 23:42:16 +0800

    2. 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ShoppingCart from "./ShoppingCart";
 import CustomersList from "./CustomersList";
 import {Route} from "react-router";
 import { BrowserRouter, Routes } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
 
 
 export default class App extends Component {
@@ -19,6 +20,7 @@ export default class App extends Component {
       <Route path="/dashboard" exact element={<Dashboard/>} />
       <Route path="customers" exact element={<CustomersList/>} />
       <Route path="/cart" exact element={<ShoppingCart/>} />
+      <Route path="*" element={<NoMatchPage/>} />
       </Routes>
       </BrowserRouter>
       
---
+++ src/NoMatchPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class NoMatchPage extends Component {
+    render(){
+        return(
+            <div>
+                <h4>Page not Found</h4>
+            </div>
+        );
+    }
+}
