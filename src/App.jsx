f09031435ee9fa02e710fe37e39fb7938e3c7c56
gerard singian <gerard.singian@outlook.com>
2021-12-08 23:35:06 +0800

    1. Basic Routing to version 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
 import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import {Route} from "react-router";
+import { BrowserRouter, Routes } from "react-router-dom";
+
 
 export default class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <NavBar />
-       <Login></Login>
-      </React.Fragment>
+      <BrowserRouter>
+       <NavBar />
+     <Routes>
+       
+      <Route path="/" exact element={<Login/>} />
+      <Route path="/dashboard" exact element={<Dashboard/>} />
+      <Route path="customers" exact element={<CustomersList/>} />
+      <Route path="/cart" exact element={<ShoppingCart/>} />
+      </Routes>
+      </BrowserRouter>
+      
     );
   }
 }
